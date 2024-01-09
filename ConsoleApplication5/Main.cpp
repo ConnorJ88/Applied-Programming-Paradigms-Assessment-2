3ac1bc27b33840924e1cf4ed22068fc08bdc380a
ConnorJ88 <connja88@gmail.com>
2024-01-09 18:08:10 +0000

    Updated some comments

--- ConsoleApplication5/Main.cpp
+++ ConsoleApplication5/Main.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 int main()
 {
+	//initialise variables
 	string userCommand;
 	vector <string> parameters;
 
@@ -22,13 +23,19 @@ int main()
 	// Create an order object
 	Order order = Order();
 
+	// continues until user inputs exit
 	while (userCommand != "exit")
 	{
+		//resets userCommand
+		userCommand = "";
 		cout << "Enter Input";
+		//gets userCommand from terminal
 		getline(cin, userCommand);
 		
 		try {
+			//ensures there is a user input
 			if (!userCommand.empty() == true) {
+				//tokenises input 
 				char* cstr = new char[userCommand.length() + 1];
 				strcpy(cstr, userCommand.c_str());
 
@@ -39,35 +46,42 @@ int main()
 					parameters.push_back(token);
 					token = strtok(NULL, " ");
 				}
-
+				//ensures parameters isn't empty
 				if (!parameters.empty()) {
+					//gets the command from parameters
 					string command = parameters[0];
-
+					//checks the command
 					if (command.compare("menu") == 0) {
+						//displays the menu from Menu::toString()
 						cout << menu.toString();
 					}
 					else if (command.compare("add") == 0)
 					{
-						Item* choice = nullptr; // you need to instantiate this using the menu object!
-
+						Item* choice = nullptr; 
+						//checks there were enough values inputted from the user
 						if (parameters.size() >= 1) {
 							try {
+								//converts the first value of parameters to an integer
 								int index = stoi(parameters[1]) - 1;
+								//instantiates the choice using menu 
 								choice = menu.getItemIndex(index);
 							}
+							//catches exception if user enters index out of range
 							catch (out_of_range& e) {
-								throw runtime_error("Invalid Input");
+								throw runtime_error("Invalid Input: Index out of range");
 							}
+							//catches exception if user enters an invalid input after add e.g add x
 							catch (invalid_argument& e) {
-								throw runtime_error("Invalid Input");
+								throw runtime_error("Invalid Input: Please enter a valid integer");
 							}
 						}
 
 						else {
 							throw runtime_error("Invalid Input");
 						}
-
+						//ensures choice isn't empty
 						if (choice != nullptr) {
+							//adds choice to the order using Order::add()
 							order.add(choice);
 						}
 
@@ -77,51 +91,62 @@ int main()
 
 					else if (command.compare("remove") == 0)
 					{
-						Item* choice = nullptr; // you need to instantiate this using the menu object!
+						Item* choice = nullptr; 
+						//checks parameters isnt empty
 						if (parameters.size() >= 1) {
 							try {
+								//attempts to convert the user input to int
 								int index = stoi(parameters[1]) - 1;
+								//gets item from the specified index
 								choice = menu.getItemIndex(index);
 							}
 							catch (out_of_range& e) {
-								throw runtime_error("Invalid Input");
+								throw runtime_error("Invalid Input: Index out of range");
 							}
 							catch (invalid_argument& e) {
-								throw runtime_error("Invalid Input");
+								throw runtime_error("Invalid Input: Please enter a valid integer");
 							}
 						}
 
 						else {
 							throw runtime_error("Invalid Input");
 						}
-
+						//ensures choice isn't a null pointer
 						if (choice != nullptr) {
+							//removes the choice from the order
 							order.remove(choice);
 						}
 					}
 					else if (command.compare("checkout") == 0)
 					{
-						string check = "";
+						//displays order and asks for confirmation
+						string check = "";				
 						cout << order.toString();
 						cout << "Do you want to place your order\n Type 'y' to checkout or 'n' to go back and modify it";
-						cin >> check;
+						getline(cin, check);
 						if ( check == "y") {
+							//calls printReceipt function to store receipt in a seperate text file
 							order.printReceipt();
+						}
+						else {
+							cout << "Continuing with order";
 						}
 					}
 					else if (command.compare("help") == 0)
 					{
+						//displays the help menu
 						string help = "";
-						help += "\t=============Checkout=============\t\n";
-						help += "\t'menu' - display the menu\n ";
-						help += "\t'add' [INDEX] - add an item to the order\n ";
-						help += "\t'remove' [INDEX] - remove item from order\n ";
-						help += "\t'checkout - display the items in the order\n ";
-						help += "\t'exit'\n";
+						help += "\t=============Help=============\t\n";
+						help += "'menu' - display the menu\n ";
+						help += "'add' [INDEX] - add an item to the order\n ";
+						help += "'remove' [INDEX] - remove item from order\n ";
+						help += "'checkout - display the items in the order\n ";
+						help += "'exit'\n";
+						cout << help;
 
 					}
 					else {
-						cout << "Invalid Input\n";
+						cout << "Invalid Input\n Type 'help' to see all commands ";
 					}
 					parameters.clear();
 
@@ -133,12 +158,13 @@ int main()
 		}
 		catch(exception& e){
 			cout << e.what() << endl;
+			parameters.clear();
 		}
 	}
 		
 		
 		
-
+	//closes the program
 	cout << "Press any key to quit...";
 	std::getchar();
 	return 0;
--- ConsoleApplication5/Order.cpp
+++ ConsoleApplication5/Order.cpp
@@ -7,6 +7,7 @@
 #include<iostream>
 #include <vector>
 #include <iomanip>
+//default constructor for order
 Order::Order() {
 	total = 0;
 	discount = 0;
@@ -16,16 +17,22 @@ pair <double, double> Order::calculateTotal(){
 	total = 0;
 	discount = 0;
 	offers = 0;
+	//calculates the total price of the order
 	for (int x = 0; x < order.size(); x++) {
+		//uses a pointer to the items price
 		total = total + order[x]->price;
 		
 	}
+	//iterates through the order to check for appetisers
 	for (int x = 0; x < order.size(); x++) {
 		if (order[x]->itemType() == 'a') {
+			//sets the item as a pointer to the appetiser class so that twoForOne can be accessed
 			const Appetiser* appetiser = dynamic_cast<const Appetiser*> (order[x]);
+			//checks if the item is part of the twoForOne offer
 			if (appetiser->twoForOne == true) {
 				offers = offers + 1;
 				offer.push_back(order[x]);
+				//if there are two offers in the offer vector it removes the lowest value from total
 				if (offers == 2){
 					if (offer[0]->price > offer[1]->price) {
 						total = total - offer[1]->price;
@@ -50,25 +57,31 @@ pair <double, double> Order::calculateTotal(){
 			}			
 		}
 	}
+	//returns both the value of total and discount as a pair
 	return make_pair( total, discount);
 }
+//writes the receipt to a file
 void Order::printReceipt() {
 	ofstream receipt("receipt.txt");
 	receipt << toString();
 }
+
 string Order::toString() {
 	pair <double, double> result = calculateTotal();
 	ostringstream p;
 	ostringstream x;
+	//formats total and discount to two decimal places
 	total = result.second;
 	p << fixed << setprecision(2) << discount;
 	discountString = p.str();
 	total = result.first;
 	x << fixed << setprecision(2) << total;
 	totalString = x.str();
+	//create a string of the checout details
 	checkout = "";
-
+	
 	checkout += "\t=======Checkout=======\t\n";
+	//adds all the items in the order to string checkout
 	for (int a = 0; a < order.size(); a++) {
 		string orderString = "";
 		orderString = order[a]->toString();
@@ -79,16 +92,34 @@ string Order::toString() {
 		checkout += "2-4-1 Discount applied! Savings: \x9c" + discountString + "\n";
 	}
 	checkout += "Total: \x9c" + totalString + "\n";
-	
+	//returns the entire string checkout ready to be outputted
 	return checkout;
 }
 void Order::add(Item* item) {
+	//adds the item to the order
 	order.push_back(item);
 	cout << item->itemName() + " added to order!\n";
 
 }
 void Order::remove(Item* item) {
-	auto it = find(order.begin(), order.end(), item);
-	order.erase(it);
-	cout << "Removed " + item->itemName() + " from order";
+		//ensures the order isn't empty before attempting to remove an item
+		
+		if (!order.empty()) {
+			//finds the item in the order
+			auto it = find(order.begin(), order.end(), item);
+			//makes sure the item is present in the order
+			if (it != order.end()) {
+				//removes the item from the order
+				order.erase(it);
+			}
+			else {
+				cout << "Item not found in order\n";
+			}
+		}
+		else {
+			cout << "Cannot remove anything from an empty order\n";
+		}
+	
+		
 }
+
